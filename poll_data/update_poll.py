5de91e623f12f3abaa542b363370f3c1ece824ac
carlosejimenez <cjsaltlake@gmail.com>
2020-10-21 18:31:04 -0400

    update update_poll.py

--- poll_data/update_poll.py
+++ poll_data/update_poll.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import wget
 
 
@@ -6,12 +7,15 @@ poll_urls = {
         'president_2020': 'https://projects.fivethirtyeight.com/polls-page/president_polls.csv',
         'senate_2020': 'https://projects.fivethirtyeight.com/polls-page/senate_polls.csv',
         'president_2020_avgs': 'https://projects.fivethirtyeight.com/2020-general-data/presidential_poll_averages_2020.csv',
-        'president_hist_avgs': 'https://github.com/fivethirtyeight/data/blob/master/polls/pres_pollaverages_1968-2016.csv',
         }
 
 
 if __name__ == '__main__':
     for poll in poll_urls.keys():
+        filepath = poll_urls[poll].split('/')[-1]
+        if os.path.exists(filepath):
+            print(f'Removing: {filepath}')
+            os.remove(filepath)
         filename = wget.download(poll_urls[poll])
         print(f'Updated {filename}')
     print(f'Done.')
